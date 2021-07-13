c4c43eb144ed68d84d3e4ec3605348a88efc37b0
Karen <karen.oflyan@gmail.com>
2021-07-14 00:17:18 +0300

    readme upd

--- README.md
+++ README.md
@@ -13,9 +13,14 @@ For example, given a nested JSON object as such
   "key2": {
     "key3": {
       "key4": "val2",
-      "key5": [1, 2, 3]
+      "key5": [1, 2, 3],
+      "key6": [
+        {
+          "key7": "val3"
+        }
+      ]
     },
-    "key5": 100
+    "key8": 100
   }
 }
 ```
@@ -24,12 +29,13 @@ The library will "unpack" it to
 
 ```json
 {
-    "key1": "val1",
-    "key2.key3.key4": "val2",
-    "key2.key3.key5[0]": 1,
-    "key2.key3.key5[1]": 2,
-    "key2.key3.key5[2]": 3,
-    "key2.key5": 100
+  "key1": "val1",
+  "key2.key3.key4": "val2",
+  "key2.key3.key5[0]": 1,
+  "key2.key3.key5[1]": 2,
+  "key2.key3.key5[2]": 3,
+  "key2.key3.key6[0].key7": "val3",
+  "key2.key8": 100
 }
 ```
 
--- test/main.test.ts
+++ test/main.test.ts
@@ -90,19 +90,25 @@ describe("Test unpack()", () => {
             key2: {
                 key3: {
                     key4: 'val2',
-                    key5: [1, 2, 3]
+                    key5: [1, 2, 3],
+                    key6: [
+                        {
+                            key7: 'val3'
+                        }
+                    ]
                 },
-                key5: 100
+                key8: 100
             },
         }
 
-        expect(unpack(o)).toEqual(   {
+        expect(unpack(o)).toEqual({
             key1: 'val1',
             'key2.key3.key4': 'val2',
             'key2.key3.key5[0]': 1,
             'key2.key3.key5[1]': 2,
             'key2.key3.key5[2]': 3,
-            'key2.key5': 100,
+            'key2.key3.key6[0].key7': 'val3',
+            'key2.key8': 100,
         });
     });
 
